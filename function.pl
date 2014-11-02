e37955203a9ec3b3321902bf7744776ea470cadc
gbingham <gbingham@bluehost.com>
2014-11-01 18:40:52 -0600

    I think I fixed it but can\'t be sure cause I\'m in a parking lot without a connection in tooele

--- function.pl
+++ function.pl
@@ -13,10 +13,12 @@ my $config = {
 };
 
 # get the full ARGV as one single string for easier regexing
-my $ssh_string = join(' ', @ARGV);
+my $ssh_string = 'ssh ' . join(' ', @ARGV);
+#print "\nmy ssh_string = $ssh_string\n";
 
 # get user and hostname together like user@hostname
 my $userhost = $ARGV[-1];
+#print "\nmy userhost = $userhost\n";
 
 # get list of files in one space seperated string
 my $files_string = '';
@@ -28,18 +30,16 @@ foreach my $item (@{$config->{'local_locations'}}) {
 my $port_string;
 my $port = $1 if $ssh_string =~ /p\s*(\d*)/;
 $port_string = ($port) ? "P $port" : "";
+#print "\nport string = $port_string\n";
 
 # do the secure copy
-print "running the following command... scp -r$port_string $files_string ${userhost}:~/";
-`scp -r $port_string $files_string ${userhost}:~/`;
+print "\ncopying your configuration files; depending upon connection this may be a few moments\n";
+print "\nrunning the following command... scp -r$port_string $files_string ${userhost}:~/\n";
+`scp -r$port_string $files_string ${userhost}:~/`;
 
 # get string to append to .bashrc 
 # my $bashrc_string = '[-z "$PS1"] && return\nexec zsh;';
 # return @ARGV; Need to ssh in perl if perl allows me to continue on in ssh without the perl script anymore
 my $bashrc_string = 'case "$-" in\n*i*) exec zsh ;;\n*) echo "" ;;\nesac';
-print "ssh string = M $ssh_string\n";
-`$ssh_string if grep "\$-" ~/.bashrc then print "all good in da hood" else echo $bashrc_string >> ~/.bashrc fi`;
-# depricated?
-    #my $at_symbol_index = index($ARGV[-1], '@') ;
-    #$hostname = substr($userhost, $at_symbol_index + 1); 
-    #$username = $1 if $userhost =~ /(\w*)@/;
+print "\nappending a configuration line to your .bashrc file";
+`$ssh_string if grep "\$-" ~/.bashrc then print "all good in da hood" else echo "$bashrc_string" >> ~/.bashrc fi`;
